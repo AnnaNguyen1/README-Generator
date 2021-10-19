2398f9d1dc0414aa3e12c7b5f4ef3fe3fe447461
Anna <an9062@gmail.com>
2021-10-19 12:51:11 +1100

    added renderLicenseBadge function

--- index.js
+++ index.js
@@ -1,15 +1,67 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const js = require("js");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Title of project:",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Description of project:",
+    },
+    {
+      type: "input",
+      name: "installinstructions",
+      message: "Installation instructions:",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Usage Information:",
+    },
+    {
+      type: "input",
+      name: "contriguidelines",
+      message: "Contribution Guidelines:",
+    },
+    {
+      type: "input",
+      name: "testinstructions",
+      message: "Test Instructions:",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Select a license for this project:",
+      choices: ["Apache", "BSD", "MIT", "Mozilla", "None"],
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your Github username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+(() => {
+  questions()
+    .then((answers) => fs.writeFileSync("README.md", generateMarkdown(answers)))
+    .then(() => console.log("Successfully created a README.md file"))
+    .catch((err) => console.error(err));
+})();
